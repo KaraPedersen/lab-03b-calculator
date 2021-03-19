0b6dd33320f84b41198f38f16a1ca4bfcc7a85fb
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-03-19 14:57:37 -0700

    refracted calculator added utils and handle

--- app.js
+++ app.js
@@ -1,66 +1,22 @@
 // import functions and grab DOM elements
-const addInput1 = document.getElementById('add-input-1');
-const addInput2 = document.getElementById('add-input-2');
+import {
+    handleAddClick,
+    handleSubtractClick,
+    handleMultiplyClick,
+    handleDivideClick,
+} from './handlers.js';
+
 const addButton = document.getElementById('add-button');
-const addAnswer = document.getElementById('add-answer');
-
-const subtractionInput1 = document.getElementById('subtraction-input-1');
-const subtractionInput2 = document.getElementById('subtraction-input-2');
-const subtractionButton = document.getElementById('subtraction-button');
-const subtractionAnswer = document.getElementById('subtraction-answer');
-
-const multiplyInput1 = document.getElementById('multiply-input-1');
-const multiplyInput2 = document.getElementById('multiply-input-2');
-const multiplyButton = document.getElementById('multiply-button');
-const multiplyAnswer = document.getElementById('multiply-answer');
-
-const divisionInput1 = document.getElementById('division-input-1');
-const divisionInput2 = document.getElementById('division-input-2');
-const divisionButton = document.getElementById('division-button');
-const divisionAnswer = document.getElementById('division-answer')
+const subtractButton = document.getElementById('subtraction-button');
+const multiplyButton = document.getElementById('multiply-button');  
+const divideButton = document.getElementById('division-button');
 
 
 // initialize state
-addButton.addEventListener('click', () => {
-
-    const value1 = addInput1.valueAsNumber;
-    const value2 = addInput2.valueAsNumber;
-
-    const sum = value1 + value2;
-
-    addAnswer.textContent = sum;
-
-
-});
-subtractionButton.addEventListener('click', () => {
-
-    const value1 = subtractionInput1.valueAsNumber;
-    const value2 = subtractionInput2.valueAsNumber;
-
-    const sum = value1 - value2;
-
-    subtractionAnswer.textContent = sum;
-});
-
-multiplyButton.addEventListener('click', () => {
-
-    const value1 = multiplyInput1.valueAsNumber;
-    const value2 = multiplyInput2.valueAsNumber;
-
-    const sum = value1 * value2;
-
-    multiplyAnswer.textContent = sum;
-});
-
-divisionButton.addEventListener('click', () => {
-
-    const value1 = divisionInput1.valueAsNumber;
-    const value2 = divisionInput2.valueAsNumber;
-
-    const sum = value1 / value2;
-
-    divisionAnswer.textContent = sum;
-});
+addButton.addEventListener('click', handleAddClick);
+subtractButton.addEventListener('click', handleSubtractClick);
+multiplyButton.addEventListener('click', handleMultiplyClick);
+divideButton.addEventListener('click', handleDivideClick);
 
 
 
